0014256ba36305017e1d5947481c309c05d5968b
Andreisr <pidecoribeiro2007@gmail.com>
2026-04-23 22:06:14 -0300

    Comentarios Adicionados

--- limpeza_dados.py
+++ limpeza_dados.py
@@ -1,40 +1,57 @@
 import pandas as pd
 import numpy as np
 
+#Transformando o arquivo .csv em DataFrame
 leitura = pd.read_csv('streamflix_raw.csv')
 
+#Analisando o DataFrame para ver possiveis correções
 print(f"O DataFrame: \n{leitura}")
 
+#Verificando se os tipos das colunas estão certos
 print(f"Os tipos de cada coluna do DataFrame:")
 leitura.info()
 
+#Analisando a quantidade de linha que tem no inicio para verificar no final
 print(f"Quantidade de linha e coluna: {leitura.shape}")
 
+#Analisando especificamente a coluna 'Data_Adesao' para ver oque tem de errado e possiveis melhoras
 print(f"Coluna 'Data_Adesao' original: \n{leitura['Data_Adesao']}")
 
+#Transfomardo a coluna 'Data_Adesao' em DataTime
 leitura['Data_Adesao'] = pd.to_datetime(leitura['Data_Adesao'], format='mixed', dayfirst=True, errors='coerce')
 
+#Verificando se sobrou algo para fazer na coluna e se está tudo certo
 print(f"Correção da coluna 'Data_Adesao': \n{leitura['Data_Adesao']}")
 
+#Analisando se tem numeros negativos na coluna 'Minutos_Assistidos'
 print(leitura[leitura['Minutos_Assistidos'] <= 0])
 
+#Se tiver coloquei para ser retirado
 leitura['Minutos_Assistidos'] = leitura['Minutos_Assistidos'].apply(lambda x: x if x >= 0 else 0)
 
+#Verificando se está tudo certo
 print(leitura[leitura['Minutos_Assistidos'] <= 0])
 
+#Analisando agora a coluna 'Plano'
 print(leitura['Plano'])
 
+#Arrumando e deixando tudo padronizado
 leitura['Plano'] = leitura['Plano'].str.title()
 
+#Verificando se deu certo
 print(leitura['Plano'])
 
+#Tirando duplicatas e apagando dados nulos
 leitura.drop_duplicates()
 leitura = leitura.dropna()
 
+#Verificando se tem dados nulos, quantas colunas foram perdidas e como ta o DataFrame
 print(leitura.isnull().sum())
 print(leitura.shape)
 print(leitura)
 
-leitura.to_csv('Streamflix_limpo.csv')
+#Salvando o DataFrame como .csv
+leitura.to_csv('Streamflix_limpo.csv', index=False)
 
+#Retorno para verificar se deu certo
 print('Salvo com sucesso!')
--- outliers.py
+++ outliers.py
@@ -2,10 +2,17 @@ import pandas as pd
 import numpy as np
 from scipy import stats
 
+#Transformando o arquivo .csv em DataFrame
 leitura = pd.read_csv('Streamflix_limpo.csv')
 
+#Criando uma coluna chamada 'z_score' para colocar todos os novos dados 
 leitura['z_score'] = np.abs(stats.zscore(leitura['Minutos_Assistidos']))
 
+#Analisando se tem outliers 
 outliers = leitura[leitura['Minutos_Assistidos'] > 3]
 
+#Verificando as possiveis oportunidades de fazer alguma coisa sobre
 print(f"Possiveis oportunidades: {len(outliers)}")
+
+#Verificando se está tudo certo
+print(leitura)
